1766dcd0952096235902a34dc9a2d70e38d2ebc4
Ilya Obretetskiy <obreil54@gmail.com>
2023-11-13 16:41:22 +0000

    Built the game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,38 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    @vowels = %w[A E Y U I O]
+    @consonants = %w[Q Z W S X D C R F V T G B H N J M K L P]
+    @letters = []
+    4.times do
+      @letters << @vowels.sample
+    end
+    6.times do
+      @letters << @consonants.sample
+    end
+    @letters.shuffle!
+  end
+
+  def score
+    @letters_in_word = params[:word].upcase.split
+    @letters = params[:letters].split(' ')
+    @score = params[:word].length
+    @letters_in_word.each do |letter|
+      if params[:letters].split(' ').include?(letter)
+        @letters.delete_at(@letters.index(letter))
+      else
+        @answer = "Sorry but #{params[:word].upcase} can't be built out of #{params[:letters].gsub(' ', ', ')}"
+      end
+      url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
+      word_serialized = URI.open(url).read
+      word_data = JSON.parse(word_serialized)
+      if word_data['found']
+        @answer = "Congratulations! #{params[:word].upcase} is a valid English word! Your score is #{@score}"
+      else
+        @answer = "Sorry but #{params[:word].upcase} does not seem to be a valid English word..."
+      end
+    end
+  end
+end
---
+++ test/controllers/new_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class NewControllerTest < ActionDispatch::IntegrationTest
+  test "should get score" do
+    get new_score_url
+    assert_response :success
+  end
+end
